ef21a54ec555cf6baca80ff4750ee0a298968529
Jonathan Torres <jonathan.torresc@outlook.com>
2015-03-28 01:13:29 -0600

    creando layout

---
+++ AgendaFinal2/app/src/main/java/com/embebidosii/uanl/edu/AcercaDe.java
@@ -0,0 +1,17 @@
+package com.embebidosii.uanl.edu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.embebidosii.uanl.edu.agendafinal.R;
+
+public class AcercaDe extends Activity  {
+
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+
+    }
+}
